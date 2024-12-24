ca93829c8042453b89256be1d8cfe3a6a144f207
Kuppu <2119916@cognizant.com>
2024-12-24 19:20:43 +0530

    add user, category, inventory tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,8 @@
 import prisma from '../lib/prisma'
 
 async function main() {
-  
+  await seedUsers();
+  await seedCategories();
 }
 main()
   .then(async () => {
@@ -12,3 +13,34 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+  
+async function seedUsers() {
+  if (!await prisma.users.findFirst({ where: { email: 'dhananjayak@yahoo.com' } })) {
+    await prisma.users.create({
+      data: {
+        email: 'dhananjayak@yahoo.com',
+        name: 'Dhananjaya',
+        mobile: '1234567890',
+      }
+    });
+  }
+}
+
+async function seedCategories() {
+  if (!await prisma.categories.findFirst({ where: { name: 'Medicine Supplies' } })) {
+    await prisma.categories.create({
+      data: {
+        name: 'Medicine Supplies',
+      }
+    });
+  }
+
+  if (!await prisma.categories.findFirst({ where: { name: 'Medicine' } })) {
+    await prisma.categories.create({
+      data: {
+        name: 'Medicine',
+      }
+    });
+  }
+}
+
